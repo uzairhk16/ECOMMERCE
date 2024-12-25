e5e0a62b27e79a3826cbe84dc9f9360331fc1170
Uzair <uzairhk16@gmail.com>
2024-12-25 17:48:42 +0530

    Update Files

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div >
+      <div className="flex flex-col sm:grid grid-cols-4 gap-14 my-10 text-sm mt-40">
+        <div>
+          <img className="mb-5 w-32" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            minima laboriosam, aspernatur hic, eveniet cupiditate ea ducimus
+            dicta quo officia, nulla delectus harum quaerat.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">HELPFUL LINKS</p>
+          <ul className="flex flex-col gap-1 text-gray-600" >
+            <li>CATEGORY</li>
+            <li>PRIVACY POLICY</li>
+            <li>REFUND POLICY</li>
+            <li>TERMS & CONDITIONS</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600" >
+            <li>HOME</li>
+            <li>COLLECTION</li>
+            <li>CONTACT US</li>
+            <li>DELIVERY</li>
+          </ul>
+        </div>
+        <div >
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+              <li className="text-xl">+1-212-456-7890</li> 
+              <li >contact@foreveryou.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <h3 className="py-5 text-md text-center">Copyright 2024© forever.com - All Rights Reserved.</h3>
+      </div>
+      
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import {ShopContext} from "../context/ShopContext"
 
 const Collection = () => {
+  const {products} = useContext(ShopContext)
+  const [showFilter, setShowFilter] = useState(false)
   return (
-    <div>Collection</div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* Filters */}
+      <div className="min-w-60">
+        <p className="flex my-2 items-center text-xl cursor-pointer gap-2">FILTERS</p>
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? "" : "hidden"} sm:block `}></div>
+      </div>
+    </div>
   )
 }
 
